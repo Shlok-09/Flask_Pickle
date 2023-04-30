64a666b4a4b8aaea939d8afbd70fe10bcd1757df
Shlok-09 <shlok.mangle@gmail.com>
2023-04-30 12:35:40 +0530

    flask

--- server/task2.py
+++ server/task2.py
@@ -1,1 +1,15 @@
 # Normal Flask Working
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return  "Home Page"
+
+@app.route('/demo')
+def demo():
+    res = {'VALUES':[11,9]}
+    return res
+
+
+app.run(host='localhost',port = 5000)
