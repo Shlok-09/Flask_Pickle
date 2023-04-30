0d1c622fe59edbde6323adfe16ba1ad057b2a3b8
Shlok-09 <shlok.mangle@gmail.com>
2023-04-30 12:52:30 +0530

    flask

--- server/task1.py
+++ server/task1.py
@@ -1,4 +1,12 @@
 # Normal Pickle Working
-# df 
-# dump 
-# load
+import pandas as pd
+import pickle
+
+rows = [['Shlok',9],['Rujuta',11],['Python',19]]
+columns = ['Name','Number']
+
+df = pd.DataFrame(rows, columns=columns)
+
+pickle.dump(df,open('demo.pkl','wb'))
+
+
--- server/task4.py
+++ server/task4.py
@@ -1,1 +1,6 @@
 # Merging
+import pickle
+
+dfload = pickle.load(open('demo.pkl','rb'))
+
+print(dfload)
